81649708f27256dc38c2274c43a53a85d4e39def
prog <prog@HAL-9000.(none)>
2010-11-05 12:51:23 +0100

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FirstApp::Application.config.secret_token = 'be269a5cbf756f34bc836946646f6049d683ec557467432f7c4e6272f620c051cf7e027ec8a8653072f5706e189a989cdab112781993bbbf61c9e3f8fc21a680'
